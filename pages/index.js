30532518426c868b45385b23472afc0d3e0b5a38
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-12 19:30:12 -0500

    set header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { headData } from "../data/data";
 import "../styles/Home.module.css";
 
 export default function Home() {
-	return <div>Hello!</div>;
+	const { title, lang, description } = headData;
+	return (
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>{title}</title>
+				<html lang={lang} />
+				<meta name="description" content={description} />
+			</Helmet>
+		</>
+	);
 }
