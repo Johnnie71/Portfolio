52c4c01601f7acb92f01712fffe6f2eff5eabb50
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-12 20:06:50 -0500

    set up state and fetching data upon page load, sending to provider

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PortfolioProvider } from "../context/context";
+import {
+	heroData,
+	aboutData,
+	projectsData,
+	contactData,
+	footerData,
+} from "../data/data";
 
 function App() {
-	return <div>Hello!</div>;
+	const [hero, setHero] = useState({});
+	const [about, setAbout] = useState({});
+	const [projects, setProjects] = useState([]);
+	const [contact, setContact] = useState({});
+	const [footer, setFooter] = useState({});
+
+	useEffect(() => {
+		setHero({ ...heroData });
+		setAbout({ ...aboutData });
+		setProjects([...projectsData]);
+		setContact({ ...contactData });
+		setFooter({ ...footerData });
+	}, []);
+
+	return (
+		<PortfolioProvider value={{ about, projects, contact, footer }}>
+			hello
+		</PortfolioProvider>
+	);
 }
 
 export default App;
