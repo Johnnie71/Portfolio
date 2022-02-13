66cf11a7c81a7b1d03e5bfa5a1127939ac4f628c
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-13 18:47:38 -0500

    imported navbar

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,19 @@ import { Helmet } from "react-helmet";
 import { headData } from "../data/data";
 import App from "../components/App";
 import "../styles/Home.module.css";
-
+import NavBar from "../components/navbar/NavBar";
 export default function Home() {
 	const { title, lang, description } = headData;
+
 	return (
 		<>
 			<Helmet>
-				<meta charSet="utf-8" />
+				<meta charSet="utf-8" name="viewport" content="width=device-width" />
 				<title>{title}</title>
 				<html lang={lang} />
 				<meta name="description" content={description} />
 			</Helmet>
+			<NavBar />
 			<App />
 		</>
 	);
