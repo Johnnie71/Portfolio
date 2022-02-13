51c7642a48cdf65d1661b2a1328d2840df82173e
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-12 19:17:25 -0500

    head data

--- data/data.js
+++ data/data.js
@@ -1,1 +1,8 @@
 import { nanoid } from "nanoid";
+
+// HEAD DATA
+export const headData = {
+	title: "Johnnie G", // e.g: 'Name | Developer'
+	lang: "en", // e.g: en, es, fr, jp
+	description: "Johnnie G Software Engineer", // e.g: Welcome to my website
+};
