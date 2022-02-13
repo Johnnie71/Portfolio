ea82bc17a9064591fec018ecaccb6ef678c2baf5
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-12 20:07:29 -0500

    added hero to provider

--- components/App.jsx
+++ components/App.jsx
@@ -24,7 +24,7 @@ function App() {
 	}, []);
 
 	return (
-		<PortfolioProvider value={{ about, projects, contact, footer }}>
+		<PortfolioProvider value={{ hero, about, projects, contact, footer }}>
 			hello
 		</PortfolioProvider>
 	);
