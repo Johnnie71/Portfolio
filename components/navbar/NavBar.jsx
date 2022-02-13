556442134ce3982fff586acf2c3cbf9df8cd46d3
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-13 18:34:09 -0500

    link item set up for navbar

---
+++ components/navbar/NavBar.jsx
@@ -0,0 +1,32 @@
+import NextLink from "next/link";
+import {
+	Container,
+	Box,
+	Link,
+	Stack,
+	Heading,
+	Flex,
+	Menu,
+	MenuItem,
+	MenuList,
+	MenuButton,
+	IconButton,
+	useColorModeValue,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const LinkItem = ({ href, path, children }) => {
+	const active = path === href;
+	const inactiveColor = useColorModeValue("gray200", "whiteAlpha.900");
+	return (
+		<NextLink href={href}>
+			<Link
+				p={2}
+				bg={active ? "turquoise" : undefined}
+				color={active ? "#202023" : inactiveColor}
+			>
+				{children}
+			</Link>
+		</NextLink>
+	);
+};
