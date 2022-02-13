e7256e6d71ec9a2f8319447509c483d70caee2ee
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-12 19:18:54 -0500

    about data

--- data/data.js
+++ data/data.js
@@ -14,3 +14,14 @@ export const heroData = {
 	subtitle: "Full stack software developer",
 	cta: "",
 };
+
+// ABOUT DATA
+export const aboutData = {
+	img: "profile.jpg",
+	paragraphOne:
+		"My name is Johnnie and I’m a full stack software developer with experience in Ruby, Ruby on Rails, JavaScript, and React. Since graduating from Flatiron Academy in December 2020, I have immersed myself in learning React Native to build a more comprehensive understanding of mobile OS applications.",
+	paragraphTwo:
+		"With a background in architectural design, and a passion for sustainability, I have discovered programming as the perfect vehicle for leading the way toward a greener future. With an estimated 3.5 billion people owning a smartphone in 2020, access to a more sustainable world is now at our fingertips.",
+	paragraphThree:
+		"Please feel free to explore my page and learn about my projects... 😀",
+};
