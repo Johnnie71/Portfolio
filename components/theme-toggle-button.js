af3f162301dabbcacae8ca866e5da773adfc3ec2
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-14 09:42:29 -0500

    toggle setup

---
+++ components/theme-toggle-button.js
@@ -0,0 +1,6 @@
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+
+const ThemeToggleIcon = () => {};
+
+export default ThemeToggleIcon;
