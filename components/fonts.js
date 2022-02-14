f927142d01385e6b89183239cbce227d27c6832c
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-14 10:16:49 -0500

    global fonts

---
+++ components/fonts.js
@@ -0,0 +1,9 @@
+import { Global } from "@emotion/react";
+
+const Fonts = () => {
+	<Global
+		styles={`@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap')`}
+	/>;
+};
+
+export default Fonts;
