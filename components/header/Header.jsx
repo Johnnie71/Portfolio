14b9a30dfd60c6114c9f4f5623b612cd612cd7d2
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-15 12:52:32 -0500

    set fading transitions for header

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -33,47 +33,63 @@ const Header = () => {
 				maxWidth={mobile ? "auto" : 800}
 			>
 				<Box borderRadius="lg" mr={10} display={{ md: "flex" }}>
-					{/* <Fade
-						top={isDesktop}
-						bottom={isMobile}
-						duration={1000}
-						delay={500}
-						distance="30px"
-					> */}
 					<Box flexGrow={1} justifyContent="center" align={"center"}>
-						<Text
-							bgGradient={useColorModeValue(
-								"linear(to-r, #1367d4, #32d142)",
-								"linear(to-l, #7928CA, #FF0080)"
-							)}
-							bgClip="text"
-							fontSize={mobile ? "5xl" : "6xl"}
-							fontWeight="extrabold"
-							ml={5}
+						<Fade
+							top={!mobile}
+							bottom={mobile}
+							duration={1000}
+							delay={500}
+							distance="30px"
 						>
-							Johnnie Gonzalez
-						</Text>
-						<Box ml={6} mb={0} align="center">
-							<p>Creative ( Artist / Engineer / For the Planet )</p>
-						</Box>
+							<Text
+								bgGradient={useColorModeValue(
+									"linear(to-r, #1367d4, #32d142)",
+									"linear(to-l, #7928CA, #FF0080)"
+								)}
+								bgClip="text"
+								fontSize={mobile ? "5xl" : "6xl"}
+								fontWeight="extrabold"
+								ml={5}
+							>
+								Johnnie Gonzalez
+							</Text>
+						</Fade>
+						<Fade
+							top={!mobile}
+							bottom={mobile}
+							duration={1000}
+							delay={900}
+							distance="30px"
+						>
+							<Box ml={6} mb={0} align="center">
+								<p>Creative ( Artist / Engineer / For the Planet )</p>
+							</Box>
+						</Fade>
 					</Box>
-					{/* </Fade> */}
 					<Box
 						flexShrink={1}
 						mt={{ base: 4, md: 4 }}
 						ml={{ md: 1 }}
 						align="center"
 					>
-						<Image
-							borderColor="whiteAplha.800"
-							borderWidth={2}
-							borderStyle="solid"
-							boxSize="120px"
-							display="inline-block"
-							borderRadius="full"
-							src="/images/profile.jpg"
-							alt="Profile Image"
-						/>
+						<Fade
+							top={!mobile}
+							bottom={mobile}
+							duration={1000}
+							delay={500}
+							distance="30px"
+						>
+							<Image
+								borderColor="whiteAplha.800"
+								borderWidth={2}
+								borderStyle="solid"
+								boxSize="120px"
+								display="inline-block"
+								borderRadius="full"
+								src="/images/profile.jpg"
+								alt="Profile Image"
+							/>
+						</Fade>
 					</Box>
 				</Box>
 			</Container>
