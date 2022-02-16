be1afbc5c16c6dc8b0bdedccae009dbbd42e0062
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-16 18:42:51 -0500

    setting up networks

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,12 +1,20 @@
 import { useContext } from "react";
 import { Container, Box, Text } from "@chakra-ui/react";
+import PortfolioContext from "../../context/context";
 
 const Footer = () => {
+	const {
+		footer: { networks },
+	} = useContext(PortfolioContext);
+
 	return (
 		<section id="footer">
-			<Container mt={20}>
+			<Container>
 				<Box>
-					<Text>FOOTER!</Text>
+					{networks &&
+						networks.map((network) => {
+							const { id, name, url } = network;
+						})}
 				</Box>
 			</Container>
 		</section>
