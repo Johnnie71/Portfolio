71ec81d65e5a48f6649212d17fe044983430423c
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-16 09:58:09 -0500

    changes

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -45,6 +45,7 @@ const Projects = () => {
 												<Box
 													color={useColorModeValue("white", "black")}
 													p={3}
+													mr={5}
 													fontWeight="bold"
 													borderRadius={20}
 													bgGradient={useColorModeValue(
@@ -57,6 +58,36 @@ const Projects = () => {
 													variant="solid"
 												>
 													Demo Video
+												</Box>
+											</Link>
+										</NextLink>
+									)}
+									{repo && (
+										<NextLink href={repo} passHref>
+											<Link
+												textDecorationStyle="none"
+												target="_blank"
+												rel="noopener noreferrer"
+												_hover={{
+													textDecorationLine: "none",
+													bgGradient: useColorModeValue("white", "black"),
+												}}
+											>
+												<Box
+													color={useColorModeValue("white", "black")}
+													p={3}
+													fontWeight="bold"
+													borderRadius={20}
+													bgGradient={useColorModeValue(
+														"linear(to-r, #1367d4, #32d142)",
+														"linear(to-l, #7928CA, #FF0080)"
+													)}
+													transition="padding .5s"
+													_hover={{ padding: 2 }}
+													as="button"
+													variant="solid"
+												>
+													Source Code
 												</Box>
 											</Link>
 										</NextLink>
