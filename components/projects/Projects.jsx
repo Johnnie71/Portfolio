29425d8199f68511bb93f3b9b945934d3d06ef5e
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-16 09:34:06 -0500

    margin top for container

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
 
 	return (
 		<section id="projects">
-			<Container>
+			<Container mt={20}>
 				<Box>
 					{projects &&
 						projects.map((project) => {
@@ -31,34 +31,36 @@ const Projects = () => {
 										<Text>{info}</Text>
 										<Text>{info2}</Text>
 									</Box>
-									<NextLink href={url} passHref>
-										<Link
-											textDecorationStyle="none"
-											target="_blank"
-											rel="noopener noreferrer"
-											_hover={{
-												textDecorationLine: "none",
-												bgGradient: useColorModeValue("white", "black"),
-											}}
-										>
-											<Box
-												color={useColorModeValue("white", "black")}
-												p={3}
-												fontWeight="bold"
-												borderRadius={20}
-												bgGradient={useColorModeValue(
-													"linear(to-r, #1367d4, #32d142)",
-													"linear(to-l, #7928CA, #FF0080)"
-												)}
-												transition="padding .5s"
-												_hover={{ padding: 2 }}
-												as="button"
-												variant="solid"
+									{url && (
+										<NextLink href={url} passHref>
+											<Link
+												textDecorationStyle="none"
+												target="_blank"
+												rel="noopener noreferrer"
+												_hover={{
+													textDecorationLine: "none",
+													bgGradient: useColorModeValue("white", "black"),
+												}}
 											>
-												Demo Video
-											</Box>
-										</Link>
-									</NextLink>
+												<Box
+													color={useColorModeValue("white", "black")}
+													p={3}
+													fontWeight="bold"
+													borderRadius={20}
+													bgGradient={useColorModeValue(
+														"linear(to-r, #1367d4, #32d142)",
+														"linear(to-l, #7928CA, #FF0080)"
+													)}
+													transition="padding .5s"
+													_hover={{ padding: 2 }}
+													as="button"
+													variant="solid"
+												>
+													Demo Video
+												</Box>
+											</Link>
+										</NextLink>
+									)}
 								</Box>
 							);
 						})}
