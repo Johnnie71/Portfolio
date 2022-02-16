d4342f8c6da426a24150e7626ad6afec1e486bd0
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-16 17:56:07 -0500

    footer set up

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+	return (
+		<section id="footer">
+			<Container mt={20}>
+				<Box>
+					<Text>FOOTER!</Text>
+				</Box>
+			</Container>
+		</section>
+	);
+};
+
+export default Footer;
