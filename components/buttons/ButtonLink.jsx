f260f032cd0098628250d77be6b57f6f837f9daf
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-16 13:27:00 -0500

    initial commit for button

---
+++ components/buttons/ButtonLink.jsx
@@ -0,0 +1,38 @@
+import { Box, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+const Button = ({ url, repo }) => {
+	return (
+		<NextLink href={url} passHref>
+			<Link
+				textDecorationStyle="none"
+				target="_blank"
+				rel="noopener noreferrer"
+				_hover={{
+					textDecorationLine: "none",
+					bgGradient: useColorModeValue("white", "black"),
+				}}
+			>
+				<Box
+					color={useColorModeValue("white", "black")}
+					p={3}
+					mr={5}
+					fontWeight="bold"
+					borderRadius={20}
+					bgGradient={useColorModeValue(
+						"linear(to-r, #1367d4, #32d142)",
+						"linear(to-l, #7928CA, #FF0080)"
+					)}
+					transition="padding .5s"
+					_hover={{ padding: 2 }}
+					as="button"
+					variant="solid"
+				>
+					Demo Video
+				</Box>
+			</Link>
+		</NextLink>
+	);
+};
+
+export default Button;
