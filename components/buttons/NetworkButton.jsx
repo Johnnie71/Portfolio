5f4951c16d8f68c084d0b8dca95c0b56830e6aaf
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-17 11:46:52 -0500

    showing icons for each social

--- components/buttons/NetworkButton.jsx
+++ components/buttons/NetworkButton.jsx
@@ -1,10 +1,41 @@
 import { Box, Link, useColorModeValue } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faGithub,
+	faLinkedin,
+	faTwitter,
+	faMedium,
+} from "@fortawesome/free-brands-svg-icons";
 
 const NetworkLink = ({ name, url }) => {
+	const getIcon = (social) => {
+		switch (social) {
+			case "twitter":
+				return faTwitter;
+				break;
+
+			case "linkedin":
+				return faLinkedin;
+				break;
+
+			case "medium":
+				return faMedium;
+				break;
+
+			case "github":
+				return faGithub;
+				break;
+
+			default:
+				return null;
+		}
+	};
+
 	return (
 		<NextLink href={url} passHref>
 			<Link
+				aria-label={name}
 				textDecorationStyle="none"
 				target="_blank"
 				rel="noopener noreferrer"
@@ -13,7 +44,7 @@ const NetworkLink = ({ name, url }) => {
 					bgGradient: useColorModeValue("white", "black"),
 				}}
 			>
-				{name}
+				<FontAwesomeIcon icon={getIcon(name)} />
 			</Link>
 		</NextLink>
 	);
