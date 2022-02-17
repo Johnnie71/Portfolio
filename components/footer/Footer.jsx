ed96d3441141adbb5c265fff9f618749f1952b86
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-16 19:02:00 -0500

    added network button component

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Container, Box, Text } from "@chakra-ui/react";
 import PortfolioContext from "../../context/context";
+import NetworkButton from "../buttons/NetworkButton";
 
 const Footer = () => {
 	const {
@@ -14,6 +15,7 @@ const Footer = () => {
 					{networks &&
 						networks.map((network) => {
 							const { id, name, url } = network;
+							<NetworkButton id={id} name={name} url={url} />;
 						})}
 				</Box>
 			</Container>
