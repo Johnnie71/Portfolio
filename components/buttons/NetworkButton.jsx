43bc92e2568ff830cf3c148af002a160e0947191
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-17 09:40:09 -0500

    removed box import

--- components/buttons/NetworkButton.jsx
+++ components/buttons/NetworkButton.jsx
@@ -1,4 +1,4 @@
-import { Box, Link, useColorModeValue } from "@chakra-ui/react";
+import { Link, useColorModeValue } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const NetworkLink = ({ name, url }) => {
