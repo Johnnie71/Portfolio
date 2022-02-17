0cf4a94fc82b34a3691239ca5c749356b005bc06
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-17 17:57:50 -0500

    title name

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,9 @@ export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="en">
-				<Head title="Johnnie G" />
+				<Head>
+					<title>Johnnie</title>
+				</Head>
 				<body>
 					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 					<Main />
