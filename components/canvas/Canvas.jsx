c551080b25364e885e6ba5103e236436cc91f67e
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-18 17:46:32 -0500

    added react suspense for fallback

--- components/canvas/Canvas.jsx
+++ components/canvas/Canvas.jsx
@@ -1,13 +1,19 @@
+import React, { Suspense } from "react";
 import { Container } from "@chakra-ui/react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import Box from "./Box";
 
 const CanvasContainer = () => {
 	return (
 		<Container mt={4} h={400} bgColor="red">
 			<Canvas className="canvas">
+				<OrbitControls />
 				<ambientLight intensity={0.5} />
-				<Box />
+				<directionalLight position={[-2, 5, 2]} intensity={1} />
+				<Suspense fallback={null}>
+					<Box />
+				</Suspense>
 			</Canvas>
 		</Container>
 	);
