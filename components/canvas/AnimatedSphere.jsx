c47c06e39b4b535bb8c31c62971b24e02a31098a
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-18 17:53:39 -0500

    sphere set up

---
+++ components/canvas/AnimatedSphere.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Sphere, MeshDistortMaterial } from "@react-three/drei";
+
+const AnimatedSphere = () => {
+	return <Sphere />;
+};
+
+export default AnimatedSphere;
