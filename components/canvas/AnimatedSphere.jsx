7489f83bd7242cb4d76fa5b322722f235f457d8d
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-19 02:24:11 -0500

    colorVar

--- components/canvas/AnimatedSphere.jsx
+++ components/canvas/AnimatedSphere.jsx
@@ -2,10 +2,12 @@ import { Sphere, MeshDistortMaterial } from "@react-three/drei";
 import { useColorModeValue } from "@chakra-ui/react";
 
 const AnimatedSphere = () => {
+	const colorTheme = useColorModeValue("#1367d4", "#8352fd");
+
 	return (
 		<Sphere rotation={[90, 0, 20]} visible args={[1, 200, 300]} scale={2}>
 			<MeshDistortMaterial
-				color={useColorModeValue("#1367d4", "#8352fd")}
+				color={colorTheme}
 				attach="material"
 				distort={0.6}
 				speed={5}
