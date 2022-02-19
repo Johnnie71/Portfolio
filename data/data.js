ca3f1ff8e038feb46d3bacbb24c842d83d2c1da8
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-18 21:12:47 -0500

    updated the data

--- data/data.js
+++ data/data.js
@@ -19,9 +19,9 @@ export const heroData = {
 export const aboutData = {
 	img: "profile.jpg",
 	paragraphOne:
-		"I’m a full stack software engineer with experience in Ruby, Ruby on Rails, JavaScript, and React. Since graduating from Flatiron Academy in December 2020, I have immersed myself in learning React Native to build a more comprehensive understanding of mobile OS applications.",
+		"I'm a software engineer with an eye for design and a love for all that is eco-friendly. I love spending time with family and friends when I can. Love to drive with the music blasting in hopes the person next to me is Shazaming my song 😂. Other than that I love to create, learn new material, and meet new people from around the world.",
 	paragraphTwo:
-		"With a background in architectural design, and a passion for sustainability, I have discovered programming as the perfect vehicle for leading the way toward a greener future. With an estimated 3.5 billion people owning a smartphone in 2020, access to a more sustainable world is now at our fingertips.",
+		"With a background in architectural design, and a passion for sustainability, I have discovered programming as the perfect vehicle for leading the way toward a greener future. With an estimated 7.26 billion people (91% of the world population) owning a smartphone in 2022, access to a more sustainable world is now at our fingertips.",
 	paragraphThree:
 		"Please feel free to explore my page and learn about my projects... 😀",
 };
