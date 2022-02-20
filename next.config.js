74817ea86f4a1111ec5ac0b787728beb771de29f
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-19 21:26:36 -0500

    testing 1 2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withVideos = require("next-videos");
+
+module.exports = withPlugins([withImages, withVideos], {
+	target: "serverless",
+});
+
 module.exports = {
 	reactStrictMode: true,
 };
