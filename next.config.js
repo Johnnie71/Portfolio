15dca83058da1c1efcd74b66428d5af940ccd803
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-19 21:28:09 -0500

    removed and added target serverless

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withVideos = require("next-videos");
-
-module.exports = withPlugins([withImages, withVideos], {
-	target: "serverless",
-});
-
 module.exports = {
 	reactStrictMode: true,
+	target: "serverless",
 };
