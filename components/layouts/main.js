28185eb1ff763f8933d5b61205c13a77d80661cf
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-21 14:10:15 -0500

    changed title for php

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,7 +6,7 @@ const Main = ({ children, router }) => {
 		<Box as="main" pb={8}>
 			<Head>
 				<meta name="viewport" content="width=device-width initial-scale=1" />
-				<title>Johnnie G - Portfolio</title>
+				<title>Johnnie G</title>
 			</Head>
 			<NavBar path={router.asPath} />
 			<Container maxW="container.md" pt={14}>
