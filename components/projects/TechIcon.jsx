924c168cb155778c7f567dc8981c5b955196333e
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-23 18:07:14 -0500

    displaying icons

--- components/projects/TechIcon.jsx
+++ components/projects/TechIcon.jsx
@@ -1,33 +1,10 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Image, Container } from "@chakra-ui/react";
 
-const TechIcon = ({ tech, key }) => {
-	console.log(tech);
-	// const getUrl = (icon) => {
-	// 	switch (icon.name) {
-	// 		case "nodejs":
-	// 			return icon.nodejs;
-
-	// 		case "react":
-	// 			return icon.react;
-
-	// 		case "ruby":
-	// 			return icon.ruby;
-
-	// 		case "postgre":
-	// 			return icon.postgre;
-
-	// 		case "javaScript":
-	// 			return icon.javaScript;
-
-	// 		case "redux":
-	// 			return icon.redux;
-	// 	}
-	// };
-
+const TechIcon = ({ name, url }) => {
 	return (
-		<Box key={key}>
-			<Image alt="hey" src="null" />
-		</Box>
+		<Container key={name}>
+			<Image alt={name} src={url} />
+		</Container>
 	);
 };
 
