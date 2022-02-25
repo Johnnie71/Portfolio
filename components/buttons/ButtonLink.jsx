e84d8d2ce7dc3f4c4a4984926366550418f7981b
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-24 22:32:31 -0500

    button padding

--- components/buttons/ButtonLink.jsx
+++ components/buttons/ButtonLink.jsx
@@ -3,35 +3,36 @@ import NextLink from "next/link";
 
 const Button = ({ url, name }) => {
 	return (
-		<NextLink href={url} passHref>
-			<Link
-				textDecorationStyle="none"
-				target="_blank"
-				rel="noopener noreferrer"
-				_hover={{
-					textDecorationLine: "none",
-					bgGradient: useColorModeValue("white", "black"),
-				}}
-			>
-				<Box
-					color={useColorModeValue("white", "black")}
-					p={3}
-					mr={name === "Email" ? "auto" : 5}
-					fontWeight="bold"
-					borderRadius={20}
-					bgGradient={useColorModeValue(
-						"linear(to-r, #1367d4, #32d142)",
-						"linear(to-l, #7928CA, #FF0080)"
-					)}
-					transition="padding .5s"
-					_hover={{ padding: 2 }}
-					as="button"
-					variant="solid"
+		<Box p={2}>
+			<NextLink href={url} passHref>
+				<Link
+					textDecorationStyle="none"
+					target="_blank"
+					rel="noopener noreferrer"
+					_hover={{
+						textDecorationLine: "none",
+						bgGradient: useColorModeValue("white", "black"),
+					}}
 				>
-					{name}
-				</Box>
-			</Link>
-		</NextLink>
+					<Box
+						color={useColorModeValue("white", "black")}
+						p={3}
+						fontWeight="bold"
+						borderRadius={20}
+						bgGradient={useColorModeValue(
+							"linear(to-r, #1367d4, #32d142)",
+							"linear(to-l, #7928CA, #FF0080)"
+						)}
+						transition="padding .5s"
+						_hover={{ padding: 2 }}
+						as="button"
+						variant="solid"
+					>
+						{name}
+					</Box>
+				</Link>
+			</NextLink>
+		</Box>
 	);
 };
 
