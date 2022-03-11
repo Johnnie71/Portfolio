25f5dd6883a0f89fe536f91e470736f61cd6097a
johnnie71 <johnnie.agonz71@gmail.com>
2022-03-10 23:53:48 -0500

    tech icons

---
+++ data/techIcons.js
@@ -0,0 +1,44 @@
+export const git = {
+	name: "git",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+};
+
+export const express = {
+	name: "express",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+};
+
+export const nodejs = {
+	name: "nodejs",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+};
+
+export const react = {
+	name: "react",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+};
+
+export const ruby = {
+	name: "ruby",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg",
+};
+
+export const rails = {
+	name: "rails",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg",
+};
+
+export const postgres = {
+	name: "postgre",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+};
+
+export const redux = {
+	name: "redux",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+};
+
+export const javaScript = {
+	name: "javaScript",
+	url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+};
