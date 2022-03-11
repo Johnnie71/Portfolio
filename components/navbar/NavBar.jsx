27b3838fea08356be5bce2aeeee34f0612df3144
johnnie71 <johnnie.agonz71@gmail.com>
2022-03-10 23:21:44 -0500

    corrected the syntax for close on select

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -91,7 +91,7 @@ const NavBar = (props) => {
 				<Box flex={1} align="right">
 					<ThemeToggleButton />
 					<Box ml={2} display={{ base: "inline-block", md: "none" }}>
-						<Menu closeOnSelect="true">
+						<Menu closeOnSelect={true}>
 							<MenuButton
 								as={IconButton}
 								icon={<HamburgerIcon />}
