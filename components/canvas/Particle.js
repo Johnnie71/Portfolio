7949eb5014882daf97e0915d772fd78177508c59
johnnie71 <johnnie.agonz71@gmail.com>
2022-04-29 00:51:32 -0400

    particle class

---
+++ components/canvas/Particle.js
@@ -0,0 +1,19 @@
+export default class Particle {
+	constructor(x, y, context) {
+		this.x = x;
+		this.y = y;
+		this.context = context;
+		this.size = 3;
+		this.baseX = this.x;
+		this.baseY = this.x;
+		this.density = Math.random() * 30 + 1;
+	}
+
+	draw() {
+		this.context.fillStyle = "red";
+		this.context.beginPath();
+		this.context.ard(this.x, this.y, this.size, 0, Math.PI * 2);
+		this.context.closePath();
+		this.context.fill();
+	}
+}
