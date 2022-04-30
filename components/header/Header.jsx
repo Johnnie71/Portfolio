c6982fd7a76c568e2316e1ba65252f404afacaca
johnnie71 <johnnie.agonz71@gmail.com>
2022-04-29 20:04:33 -0400

    moved particle up for testing

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -17,6 +17,7 @@ const Header = () => {
 	return (
 		<section id="header">
 			<CanvasContainer mobile={mobile} color={colorTheme} />
+			<ParticleFont />
 			<Container
 				// borderStyle="solid"
 				// borderWidth="2px"
@@ -45,7 +46,6 @@ const Header = () => {
 							>
 								Johnnie Gonzalez
 							</Text>
-							<ParticleFont />
 						</Fade>
 						<Fade
 							top={!mobile}
