6a6a525089c3ad78ca0bda0104cad420f1c119c7
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-03 20:43:26 -0400

    hiding nav bar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -20,7 +20,7 @@ const Main = ({ children, router }) => {
 			<Container maxW="98%" pt={14}>
 				{children}
 			</Container>
-			{isMobile ? <BottomNavBar /> : null}
+			{/* {isMobile ? <BottomNavBar /> : null} */}
 		</Box>
 	);
 };
