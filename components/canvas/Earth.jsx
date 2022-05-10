c23488d22bfd29d20fe0754db5699528cff599f7
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-10 19:20:46 -0400

    using mesh

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 
-const Earth = () => {};
+const Earth = () => {
+	return (
+		<Canvas>
+			<mesh></mesh>
+		</Canvas>
+	);
+};
 
 export default Earth;
