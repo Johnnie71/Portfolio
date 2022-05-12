76ee39a65d10bc4e7e096c112aab08652094f11c
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-12 19:31:27 -0400

    created canvas

--- components/canvas/CarCanvas/Car.jsx
+++ components/canvas/CarCanvas/Car.jsx
@@ -1,5 +1,12 @@
 import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 
-const CarCanvas = () => {};
+const CarCanvas = () => {
+	return (
+		<Suspense fallback={null}>
+			<Canvas shadows></Canvas>
+		</Suspense>
+	);
+};
 
 export default CarCanvas;
