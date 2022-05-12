c838a823af67b1d77eadd72cbd9227c54a9b50f5
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-11 20:36:35 -0400

    changed directional light

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -4,9 +4,9 @@ import { Canvas } from "@react-three/fiber";
 const Earth = () => {
 	return (
 		<Canvas>
+			<ambientLight intensity={0.1} />
+			<directionalLight color="red" position={[0, 0, 5]} />
 			<mesh>
-				<ambientLight intensity={0.1} />
-				<directionalLight color="red" position={[0, 0, 5]} />
 				<circleBufferGeometry attach="geometry" args={[2, 200]} />
 				<meshStandardMaterial attach="material" />
 			</mesh>
