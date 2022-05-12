d40dcd565794b5bcbbf05c72d05ec8d02fd94901
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-12 19:39:42 -0400

    mesh, boxgeometry, meshbasicmaterial

--- components/canvas/CarCanvas/Car.jsx
+++ components/canvas/CarCanvas/Car.jsx
@@ -7,6 +7,10 @@ const CarShow = () => {
 		<>
 			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
 			<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+			<mesh>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshBasicMaterial color={"red"} />
+			</mesh>
 		</>
 	);
 };
