71d67d463eeba9dd80a1a917a7e9adf927e6205e
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-12 19:32:19 -0400

    created car function

--- components/canvas/CarCanvas/Car.jsx
+++ components/canvas/CarCanvas/Car.jsx
@@ -1,6 +1,10 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 
+const CarShow = () => {
+	return null;
+};
+
 const CarCanvas = () => {
 	return (
 		<Suspense fallback={null}>
