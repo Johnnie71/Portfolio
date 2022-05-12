f47d470105659d89343f1666b829362cacc7df8b
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-11 20:33:31 -0400

    setting up light for circle

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -5,6 +5,8 @@ const Earth = () => {
 	return (
 		<Canvas>
 			<mesh>
+				<ambientLight intensity={0.1} />
+				<directionalLight color="red" position={[0, 0, 5]} />
 				<circleBufferGeometry attach="geometry" args={[2, 200]} />
 				<meshStandardMaterial attach="material" />
 			</mesh>
