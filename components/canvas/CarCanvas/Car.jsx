8233dc9f02a1e0748345819f50c9162bc7b306f1
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-14 16:46:07 -0400

    react three fiber

--- components/canvas/CarCanvas/Car.jsx
+++ components/canvas/CarCanvas/Car.jsx
@@ -7,10 +7,10 @@ const CarShow = () => {
 		<>
 			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
 			<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
-			<mesh>
+			{/* <mesh>
 				<boxGeometry args={[1, 1, 1]} />
 				<meshBasicMaterial color={"red"} />
-			</mesh>
+			</mesh> */}
 		</>
 	);
 };
