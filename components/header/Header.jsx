39093ef99cf13f993d18d5c106fd833f0f969fdc
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-16 18:49:16 -0400

    imported threejs test

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -8,6 +8,7 @@ import {
 } from "@chakra-ui/react";
 import { Fade } from "react-reveal";
 import CanvasContainer from "../canvas/Canvas";
+import CarCanvas from "../canvas/CarCanvas/Car";
 
 const Header = () => {
 	const [mobile] = useMediaQuery("(max-width: 768px)");
@@ -15,6 +16,8 @@ const Header = () => {
 
 	return (
 		<section id="header">
+			<CarCanvas />
+
 			<CanvasContainer mobile={mobile} color={colorTheme} />
 			<Container
 				height={!mobile ? "56vh" : "35vh"}
