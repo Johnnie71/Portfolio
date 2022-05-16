1fbd6e99ae53018d595cfc9c75ea9ec3e68da629
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-16 18:54:31 -0400

    added dithering

--- components/canvas/CarCanvas/Ground.jsx
+++ components/canvas/CarCanvas/Ground.jsx
@@ -4,7 +4,7 @@ export function Ground() {
 	return (
 		<mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
 			<planeGeometry args={[30, 30]} />
-			<MeshReflectorMaterial envMapIntensity={0} />
+			<MeshReflectorMaterial envMapIntensity={0} dithering={true} />
 		</mesh>
 	);
 }
