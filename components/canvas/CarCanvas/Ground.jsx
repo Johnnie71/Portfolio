7984caeee0c76e03e7a866b9315bca4c97f8a6d3
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-16 18:55:26 -0400

    added color

--- components/canvas/CarCanvas/Ground.jsx
+++ components/canvas/CarCanvas/Ground.jsx
@@ -4,7 +4,11 @@ export function Ground() {
 	return (
 		<mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
 			<planeGeometry args={[30, 30]} />
-			<MeshReflectorMaterial envMapIntensity={0} dithering={true} />
+			<MeshReflectorMaterial
+				envMapIntensity={0}
+				dithering={true}
+				color={[0.015, 0.015, 0.015]}
+			/>
 		</mesh>
 	);
 }
