c047226a42c55b4ee20a30786356bb6b15cf2fb2
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-16 18:51:39 -0400

    added properties and shadowing to mesh

--- components/canvas/CarCanvas/Ground.jsx
+++ components/canvas/CarCanvas/Ground.jsx
@@ -2,7 +2,7 @@ import { MeshReflectorMaterial } from "@react-three/drei";
 
 export function Ground() {
 	return (
-		<mesh>
+		<mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
 			<planeGeometry />
 			<MeshReflectorMaterial />
 		</mesh>
