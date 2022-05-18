9c46f23a046ccc70f25ba4f0ba508f12c1dc7ebf
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-18 19:23:23 -0400

    added mesh

--- components/canvas/CarCanvas/Car.jsx
+++ components/canvas/CarCanvas/Car.jsx
@@ -8,10 +8,10 @@ const CarShow = () => {
 		<>
 			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
 			<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
-			{/* <mesh>
+			<mesh>
 				<boxGeometry args={[1, 1, 1]} />
 				<meshBasicMaterial color={"red"} />
-			</mesh> */}
+			</mesh>
 			<color args={[0, 0, 0]} attach="background" />
 			<spotLight
 				color={[1, 0.25, 0.7]}
