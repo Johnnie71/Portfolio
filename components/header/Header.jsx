fe52f58f472a9aa7c6ba5c537f974b44a8755373
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-26 21:57:04 -0400

    removed import

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -8,7 +8,6 @@ import {
 } from "@chakra-ui/react";
 import { Fade } from "react-reveal";
 import CanvasContainer from "../canvas/Canvas";
-import WorldCanvas from "../canvas/WorldCanvas";
 
 const Header = () => {
 	const [mobile] = useMediaQuery("(max-width: 768px)");
