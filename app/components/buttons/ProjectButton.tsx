01eaa7dae8fae856ba78c6c614c86534de7d63ca
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-11-14 15:15:30 -0500

    smoothed transition on button hover

--- app/components/buttons/ProjectButton.tsx
+++ app/components/buttons/ProjectButton.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 interface Props {
   url: string,
   text: string
-}
+} 
 
 const ProjectButton: React.FC<Props> = ({ url, text }) => {
   return (
-    <button className="flex gap-3 cursor-pointer text-black font-semibold bg-gradient-to-r from-primary to-secondary px-7 py-3 rounded-full hover:scale-105 duration-200 transition-colors hover:border-gray-800 hover:from-secondary hover:to-primary shadow-lg shadow-black">
-      <a href={url} title="Demo Video">{text}</a>
+    <button className="flex gap-3 cursor-pointer text-black font-semibold bg-gradient-to-r from-primary to-secondary px-7 py-3 rounded-full hover:scale-105 duration-300 transition-all ease-in-out hover:border-gray-800 hover:from-secondary hover:to-primary shadow-lg shadow-black">
+      <a href={url} title={`${text}`} target='_blank'>{text}</a>
     </button>
   )
 }
